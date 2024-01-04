61e1764965a51d757c4c825585be118b2d8333d1
abc <abc@gmail.com>
2024-01-03 22:43:46 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RainbowSchoolStudentsData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "studentdata.txt";
+            try
+            {
+                string[] studentData = File.ReadAllLines("C:\\Users\\lenovo\\OneDrive\\Desktop\\Project 2\\studentdata.txt");
+                foreach (var student in studentData)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"File not found: {filepath}");
+            }
+        }
+    }
+}
